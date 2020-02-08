77a74ba9c57527547760204edbc3554f9b54dbe8
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-02-08 11:50:20 -0600

    first commit to new repo because i messed up and deleted the last one

---
+++ auto_test_graph/linegraph.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart"),
+    [{ data: co2, lines: {show:true} }],
+    document.getElementById("chart1"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
+
+/*window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart1"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
+*/
+window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart2"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
+
+window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart3"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
+
+window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart4"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
+
+window.onload = function () {
+  Flotr.draw(
+    document.getElementById("chart5"),
+    [{ data: co2, lines: {show:true} }]
+  );
+}
---
+++ js_array_maker.py
@@ -0,0 +1,5 @@
+# input file title/name
+
+name_of_file = input('<<< type name of js data file >>>\t')
+
+print(name_of_file + '.js')
